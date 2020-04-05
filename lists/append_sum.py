1e2d3b50bb8af61bd377ba93da55e04d61f1db8f
Andrey <terehovandrej@gmail.com>
2020-04-05 17:19:56 +0300

    new exersices

--- lists/append_sum.py
+++ lists/append_sum.py
@@ -2,3 +2,14 @@
 # The function should add the last two elements of lst together and append the result
 # to lst. It should do this process three times and then return lst.
 # For example, if lst started as [1, 1, 2], the final result should be [1, 1, 2, 3, 5, 8].
+
+
+def append_sum(lst):
+    counter = 0
+    while counter < 3:
+        lst.append(sum(lst[-2:]))
+        counter += 1
+    return lst
+
+
+print(append_sum([1, 1, 2]))
